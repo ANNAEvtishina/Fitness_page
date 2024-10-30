82a49d9286e1a409c2fbff1ca4ef6242506f36c6
ANNA EVTISHINA <a.evtishina.i@mail.ru>
2024-10-31 00:03:16 +0300

    change about page, add trainer people

--- about/about.js
+++ about/about.js
@@ -6,11 +6,3 @@ burgerMenu.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
-let count = 0;
-const button = document.getElementById("click-button"); // найдите элементы с помощью getElement или аналогии
-const counterDisplay = document.getElementById("counter");
-
-button.addEventListener("click", function () {
-  count += 1;
-  counterDisplay.textContent = count; // Увеличьте значение count и обновите текст элемента
-});
--- trainers/trainers.js
+++ trainers/trainers.js
@@ -1,3 +1,5 @@
+
+
 console.log('Скрипт загружен');
 
 const burgerMenu = document.querySelector('.burger-menu');
@@ -13,4 +15,25 @@ const counterDisplay = document.getElementById("counter");
 button.addEventListener("click", function () {
   count += 1;
   counterDisplay.textContent = count; // Увеличьте значение count и обновите текст элемента
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.like-button').forEach(button => {
+        button.addEventListener('click', async () => {
+            const id = button.getAttribute('data-id');
+            const response = await fetch('update_likes.php', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id: id })
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                document.getElementById(`counter-${id}`).innerText = result.likes;
+            } else {
+                console.error('Ошибка обновления лайков');
+            }
+        });
+    });
 });
